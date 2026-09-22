92c1ca879a97b263f13dbd70fc0d8710149423d6
TheTaniaAkter <tania.hussain214@gmail.com>
2026-09-22 21:11:49 +0600

    add color

--- src/components/AvailableItems.jsx
+++ src/components/AvailableItems.jsx
@@ -10,17 +10,50 @@ const AvailableItems = ({item}) => {
        item.map((item)=>{
 return <div className="card bg-base-100 w-96 shadow-sm  ">
    
-  {/* <figure>
   
-    <img
-      src={item.icon} className='w-12 h-12'
-      alt="" />
-  </figure> */}
+  <div className="flex justify-between">
+     <img src={item.icon} alt="" className='w-9 ml-5 mt-5' />
+      {/* <h3 >{item.badge}</h3> */}
+      {/* ========================================= */}
+      <span
+  className={`px-3 py-1 text-[10px] font-medium mt-4 rounded-2xl pt-3
+    ${
+      item.badge === "Popular"
+        ? "bg-blue-50 text-blue-500"
+        : item.badge === "Versatile"
+        ? "bg-green-50 text-green-500"
+        : item.badge === "Fast"
+        ? "bg-orange-50 text-orange-500"
+        : item.badge === "SSR / Edge"
+        ? "bg-purple-50 text-purple-500"
+        : item.badge === "Robust"
+        ? "bg-blue-50 text-blue-500"
+        : item.badge === "Standard"
+        ? "bg-green-50 text-green-500"
+        : item.badge === "Top SQL"
+        ? "bg-blue-50 text-blue-500"
+        : item.badge === "Cache"
+        ? "bg-red-50 text-red-500"
+        : item.badge === "Ubiquitous"
+        ? "bg-yellow-50 text-yellow-600"
+        : item.badge === "Essential"
+        ? "bg-blue-50 text-blue-500"
+        : item.badge === "Modern"
+        ? "bg-cyan-50 text-cyan-500"
+        : item.badge === "Containers"
+        ? "bg-blue-50 text-blue-500"
+        : "bg-gray-50 text-gray-500"
+    }`}
+>
+  {item.badge}
+</span>
+      {/* ================================================ */}
+  </div>
   <div className="card-body">
    <div className="flex justify-between">
-    <h3>{item.icon}</h3>
+   
   
-    <h3>{item.badge}</h3>
+   
    </div>
     <h2 className="card-title font-jakartaSans font-bold text-[18px] text-black">{item.name}</h2>
     <p className='ptext  text-cm text-[12px] font-jakartaSans'>{item.description}</p>
--- src/components/layouts/Bottom.jsx
+++ src/components/layouts/Bottom.jsx
@@ -6,39 +6,44 @@ const Bottom = () => {
   return (
     <>
    <Container>
-   <div className="bg-gray-300 py-[100px] flex justify-between mt-5">
-    <div className="">
+   <div className="py-[100px] flex justify-between mt-5">
+     <div className="">
 <Image imgSrc={logo} className='font-bold font-jakartaSans text-[18px]'/>
 <p className='w-[378px] font-jakartaSans text-[12px] text-cm leading-[19.5px]'>Curated tools, technologies, and resources for developers building
 modern software.</p>
-<div className="flex gap-x-3">
+<div className="flex gap-x-3 pt-4">
     <h6>GitHub</h6>
     <h6>Twitter</h6>
     <h6>LinkedIn</h6>
    
 </div>
- <p>© 2026 Dev Stack. All rights reserved.</p>
+<p className='mt-24 font-jakartaSans text-[12px] text-cm'>© 2026 Dev Stack. All rights reserved.</p>
     </div>
     <div className="">
-        <h5 className='font-bold font-jakartaSans text-[12px]'>PRODUCT</h5>
-        <p className='font-jakartaSans text-[12px] text-cm'>Home</p>
-        <p className='font-jakartaSans text-[12px] text-cm'>Technologies</p>
+        <h5 className='font-bold font-jakartaSans text-[12px] pb-3'>PRODUCT</h5>
+        <p className='font-jakartaSans text-[12px] text-cm pb-2'>Home</p>
+        <p className='font-jakartaSans text-[12px] text-cm pb-2'>Technologies</p>
         <p className='font-jakartaSans text-[12px] text-cm '>Projects</p>
     </div>
     <div className="">
-        <h5 className='font-bold font-jakartaSans text-[12px]'>COMPANY</h5>
-        <p className='font-jakartaSans text-[12px] text-cm'>About</p>
-        <p className='font-jakartaSans text-[12px] text-cm'>Contact</p>
-        <p className='font-jakartaSans text-[12px] text-cm'>Careers</p>
+        <h5 className='font-bold font-jakartaSans text-[12px] pb-3'>COMPANY</h5>
+        <p className='font-jakartaSans text-[12px] text-cm pb-2'>About</p>
+        <p className='font-jakartaSans text-[12px] text-cm pb-2'>Contact</p>
+        <p className='font-jakartaSans text-[12px] text-cm '>Careers</p>
        
     </div>
     <div className="">
-        <h5 className='font-bold font-jakartaSans text-[12px]'>LEGAL</h5>
-        <p className='font-jakartaSans text-[12px] text-cm'>Privacy Policy</p>
-        <p className='font-jakartaSans text-[12px] text-cm'>Terms of Service</p>
+        <h5 className='font-bold font-jakartaSans text-[12px] pb-3'>LEGAL</h5>
+        <p className='font-jakartaSans text-[12px] text-cm pb-2'>Privacy Policy</p>
+        <p className='font-jakartaSans text-[12px] text-cm pb-10'>Terms of Service</p>
+       <div className="flex gap-x-3 mt-24 font-jakartaSans text-[12px] text-cm">
+         <p>Privacy</p>
+        <p>Terms</p>
+       </div>
     </div>
+
    </div>
-  
+
    </Container>
     </>
   )

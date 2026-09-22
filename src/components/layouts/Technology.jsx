78722341a6a3e8b75af74e85d0743ad9558b1135
TheTaniaAkter <tania.hussain214@gmail.com>
2026-09-22 21:31:11 +0600

    add badge

--- src/components/layouts/Technology.jsx
+++ src/components/layouts/Technology.jsx
@@ -9,7 +9,7 @@ const Technology = () => {
   <>
  <div className="py-6 ">
     <Container >
-         <h3 className='font-interSans text-black font-extrabold text-[36px] '>Explore the <span className='text-[#EC4899]'>Technologies</span> </h3>
+         <h3 className='font-interSans text-black font-extrabold text-[36px] '>Explore the <span className="bg-gradient-to-r from-[#F97316] to-[#EC4899] bg-clip-text text-transparent" >Technologies</span> </h3>
      <p className='text-cm text-[16px]'>Pick one technology per category to build your ideal stack.</p>
     
   
